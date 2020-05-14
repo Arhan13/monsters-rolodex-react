03baa7888072509aeecf9f41fe81b8d33b311fbc
Arhan13 <60483503+Arhan13@users.noreply.github.com>
2020-05-14 06:50:28 +0530

    Updates

---
+++ precache-manifest.f441e1d93ea3da46094bd154cbab242c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6c2bbc00eeac5a7dd0e2881de82a7f",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "9b12c4964476a5444a04",
+    "url": "/monsters-rolodex-react/static/css/main.15a3fece.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex-react/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b12c4964476a5444a04",
+    "url": "/monsters-rolodex-react/static/js/main.2916e1ae.chunk.js"
+  },
+  {
+    "revision": "865d99636306d4f651c4",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.b3bf97e2.js"
+  }
+]);
